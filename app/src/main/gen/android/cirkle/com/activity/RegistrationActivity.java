26f157dd0cb1d805ec2986ea9a11df3ed8eda8db
MohamedKhorshid <mohamedwagdykhorshid@gmail.com>
2015-01-03 21:47:46 +0200

    Added new Activity

---
+++ app/src/main/gen/android/cirkle/com/activity/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package android.cirkle.com.activity;
+
+import android.app.Activity;
+import android.cirkle.com.R;
+import android.os.Bundle;
+
+/**
+ * Created by Dodo on 1/3/2015.
+ */
+public class RegistrationActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+    }
+}

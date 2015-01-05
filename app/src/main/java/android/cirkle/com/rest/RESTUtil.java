a5389f30ae9137c2c784bb593f882fd27a80d2c1
Mohamed Wagdy <mohamedwagdykhorshid@gmail.com>
2015-01-05 09:31:56 +0200

    Android registration screen

---
+++ app/src/main/java/android/cirkle/com/rest/RESTUtil.java
@@ -0,0 +1,11 @@
+package android.cirkle.com.rest;
+
+/**
+ * Created by Mohamed Wagdy on 1/5/2015
+ */
+public class RESTUtil {
+
+    public static String getServerConnectionString() {
+        return "http://cirkle-nodejs.herokuapp.com";
+    }
+}
